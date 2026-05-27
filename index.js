56860782b8e0ca839be75df0423d3503867855d2
WalterMIM <waltermim01@gmail.com>
2026-05-27 16:47:05 -0400

    dea2

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -14,19 +13,3 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Servidor en: http://localhost:${PORT}`);
 });
-=======
-
-const http = require('http');
-const express = require('express');
-const app = express();
-
-app.use(express.static(__dirname + '/'));
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
-});
-
-app.listen(4000);
-console.log('Servidor en puerto 4000');
->>>>>>> e5570584487b126fc7b7f3fc3d421fd6abaf1115
