bbd22a189f172192e1912f2913fd8c6539a1ea3f
GausandJordan <juandiegofloresb@gmail.com>
2026-05-30 09:18:59 -0400

    Cartelera.html enlazada con nav, icono de pesta;a

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
